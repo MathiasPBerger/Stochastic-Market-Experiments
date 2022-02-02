40e373a891d68637ff88a061294ef5bb705f088f
Mathias Berger <mathias.berger@uliege.be>
2022-02-02 18:26:32 -0500

    First draft of implementation.

--- paper_counter_example.jl
+++ paper_counter_example.jl
@@ -6,36 +6,50 @@ using Distributions
 ## Data
 
 # Number of flexible generators and wind farms
-n_g = 10;
-n_w = 6;
+n_g = 3;
+n_w = 1;
 
 # Flexible generation parameters
-p_g_max = 200.0
-p_g_min = 0.0
+p_max = 200.0;
+p_min = 0.0;
+R_D_max = 200.0;
+R_U_max = 200.0;
 
 # Wind production parameters
-p_w_max = 200.0;
-std = 200*0.075;
+p_w_max = 20.0;
+std = p_w_max*0.075;
 var = std^2;
 cov_mat = var .* convert.(Float64, Matrix(I, n, n));
 cov = sum(cov_mat);
-W =
+agg_cov = sqrt(cov);
+W = 0.8*p_w_max*n_w;
 
 # Demand parameters
-D = 1000
+D = 1000;
 
 # Risk parameters
-epsilon = 0.05
-
+epsilon = 0.05;
+phi = quantile(Normal(), (1-epsilon));
 
 # Cost parameters
-C_Q = [10 for i = 1:n]
-C_L = [3 for i = 1:n]
+v_Q, s_Q = 0.1, 0.05;
+v_L, s_L = 1.0, 0.1;
+C_Q = [(v_Q + s_Q*g) for g = 1:n];
+C_L = [(v_L + s_L*g) for g = 1:n];
 
 
 ## Model
 
 model = Model(Gurobi.Optimizer)
 
-@variable(model, p_g[i = 1:n] .>= 0)
-@variable(model, alpha_g[1=1:n] .>= 0)
+@variable(model, 0.0 <= p[g = 1:n])
+@variable(model, 0.0 <= alpha[g = 1:n] <= 1.0)
+
+@constraint(model, power_balance, sum(p[g] for g = 1:n) + W == D)
+@constraint(model, reserve_allocation, sum(alpha[g] for g = 1:n) == 1.0)
+@constraint(model, min_prod[g = 1:n], p_min[g] <= p[g] - phi*agg_cov*alpha[g])
+@constraint(model, max_prod[g = 1:n], p[g] + phi*agg_cov*alpha[g] <= p_max[g])
+@constraint(model, down_ramp[g = 1:n], -R_D_max <= phi*agg_cov*alpha[g])
+@constraint(model, up_ramp[g = 1:n], phi*agg_cov*alpha[g] <= R_U_max)
+
+@objective(model, Min, sum(C_Q[g]*(p[g]^2 + cov*alpha[g]^2) + C_L[g]*p[g] for g = 1:n))
