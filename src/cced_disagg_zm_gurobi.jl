ed2d9357e48a7aa06635e1474113314c2dda5d52
Mathias Berger <mathias.berger@uliege.be>
2022-04-15 23:18:23 -0400

    Corrected post-processing for zero mean cases.

--- src/cced_disagg_zm_gurobi.jl
+++ src/cced_disagg_zm_gurobi.jl
@@ -1,22 +1,25 @@
 using JuMP
+using Gurobi
 using LinearAlgebra
 using Distributions
-using Ipopt
 
 ## Comments
 #
 # This script formulates the problem as a convex program with a convex quadratic
-# objective to be minimised subject to affine and second-order cone constraints.
-# It then solves the problem with Ipopt. This has non-trivial implications
+# objective to be minimised subject to affine and quadratic constraints.
+# It then solves the problem with Gurobi. This has non-trivial implications
 # for post-processing because of the primal and dual forms used by the solver.
-# More precisely, Ipopt relies on Lagrangian duality, which means that the dual
-# variables associated with second-order cone constraints are scalar variables.
-# Theoretical developments are also based on Lagrangian duality and the
-# formulation used in the theoretical analysis can be solved as such, which
-# implies that the meaning of dual variables of second-order cone constraints
-# retrieved from the solver is roughly the same (they are the opposite of the
-# ones used in our developments). They can therefore be used to check our
-# results numerically.
+# More precisely, Gurobi seems to use Lagragian duality, as the dual variables
+# associated with quadratic (second-order cone) constraints are scalar.
+# Theoretical results in the tex documents were derived based on Lagrangian
+# duality. The formulation used in the theoretical analysis had to be recast
+# to be solved by Gurobi (i.e., turning the second-order cone constraints
+# into quadratic constraints), however, which has implications for the
+# interpretation of dual variables. Indeed, the dual variables returned by
+# Gurobi do not have the exact same meaning as the ones used in our theoretical
+# developments, and some slight discrepancy should be expected between
+# theoretical and empirical results when these variables have nonzero values (
+# i.e., when generation bound constraints are tight).
 
 ## Data
 
@@ -70,7 +73,6 @@ if truthful_bidding == true
     C_L = deepcopy(true_C_L);
 else
     fake_C_L = zeros(Float64, n_g);
-    fake_C_L[n_g] += 1.0;
     C_Q = deepcopy(true_C_Q);
     C_L = deepcopy(true_C_L) .+ fake_C_L;
 end
@@ -78,23 +80,20 @@ end
 
 ## Model
 
-NL_model = true # setting it to true defines a model that is exactly identical to the one used in our theoretical developments (guaranteeing consistency of dual variables)
-
-model = Model(Ipopt.Optimizer)
+model = Model(Gurobi.Optimizer)
+set_optimizer_attribute(model, "QCPDual", 1)
 
 @variable(model, 0.0 <= p[g = 1:n_g])
 @variable(model, 0.0 <= alpha[g = 1:n_g, i = 1:n_w] <= 1.0)
+@variable(model, 0 <= s_max[g = 1:n_g]) # auxiliary variable defining LHS slacks (required to bring problem to form recognised by Gurobi)
+@variable(model, 0 <= s_min[g = 1:n_g]) # auxiliary variable defining LHS slacks of quadratic constraint (required to bring problem to form recognised by Gurobi)
 
 @constraint(model, power_balance, sum(p[g] for g = 1:n_g) + sum(W[i] for i = 1:n_w) + sum(mu[i] for i = 1:n_w) == D)
 @constraint(model, reserve_allocation[i = 1:n_w], sum(alpha[g, i] for g = 1:n_g) == 1.0)
-if NL_model == true
-    @expression(model, qt[g = 1:n_g], alpha[g,:]'*cov_mat*alpha[g, :])
-    @NLconstraint(model, max_prod[g = 1:n_g], phi * sqrt(qt[g]) <= p_max[g] - p[g])
-    @NLconstraint(model, min_prod[g = 1:n_g], phi * sqrt(qt[g]) <= p[g] - p_min[g])
-else
-    @constraint(model, max_prod[g = 1:n_g], phi^2 * alpha[g,:]'*cov_mat*alpha[g, :] <= (p_max[g] - p[g])^2)
-    @constraint(model, min_prod[g = 1:n_g], phi^2 * alpha[g,:]'*cov_mat*alpha[g, :] <= (p[g] - p_min[g])^2)
-end
+@constraint(model, aff_expr_slack_max[g = 1:n_g], s_max[g] == p_max[g] - p[g])
+@constraint(model, aff_expr_slack_min[g = 1:n_g], s_min[g] == p[g] - p_min[g])
+@constraint(model, max_prod[g = 1:n_g], phi^2 * alpha[g,:]'*cov_mat*alpha[g,:] <= s_max[g]^2)
+@constraint(model, min_prod[g = 1:n_g], phi^2 * alpha[g,:]'*cov_mat*alpha[g,:] <= s_min[g]^2)
 
 @objective(model, Min, sum(C_Q[g]*(p[g]^2 + alpha[g,:]'*cov_mat*alpha[g,:]) + C_L[g]*p[g] for g = 1:n_g))
 
@@ -111,30 +110,36 @@ p_scheduled = value.(p);
 alpha_scheduled = value.(alpha);
 electricity_price = dual(power_balance);
 reserve_price = dual.(reserve_allocation);
-dual_min_prod = abs.(dual.(min_prod)); # absolute value is taken since Ipopt does not seem to use the same sign convention for dual variables (it is negative when it should be positive)
-dual_max_prod = abs.(dual.(max_prod)); # absolute value is taken since Ipopt does not seem to use the same sign convention for dual variables (it is negative when it should be positive)
+dual_min_prod = abs.(dual.(min_prod)); # absolute value is taken since Gurobi does not seem to use the same sign convention for dual variables (it is negative when it should be positive)
+dual_max_prod = abs.(dual.(max_prod)); # absolute value is taken since Gurobi does not seem to use the same sign convention for dual variables (it is negative when it should be positive)
 load_payment = D*electricity_price;
 wind_energy_revenue = [W[i]*electricity_price for i = 1:n_w];
 wind_profit = [(wind_energy_revenue[i]-reserve_price[i]) for i = 1:n_w];
 generator_energy_revenue = [(electricity_price*p_scheduled[g]) for g = 1:n_g];
 generator_reserve_revenue = [(reserve_price'*alpha_scheduled[g,:]) for g = 1:n_g];
-true_generator_cost = [(true_C_Q[g]*(p_scheduled[g]^2 + alpha_scheduled[g,:]'*cov_mat*alpha_scheduled[g,:]) + true_C_L[g]*p_scheduled[g]) for g = 1:n_g];
+true_generator_cost = [(true_C_Q[g]*(p_scheduled[g]^2 + alpha_scheduled[g,:]'*cov_mat*alpha_scheduled[g,:]) + true_C_L[g]*p_scheduled[g] ) for g = 1:n_g];
 reported_generator_cost = [(C_Q[g]*(p_scheduled[g]^2 + alpha_scheduled[g,:]'*cov_mat*alpha_scheduled[g,:]) + C_L[g]*p_scheduled[g]) for g = 1:n_g];
 true_generator_profit = [(generator_energy_revenue[g] + generator_reserve_revenue[g] - true_generator_cost[g]) for g = 1:n_g];
 reported_generator_profit = [(generator_energy_revenue[g] + generator_reserve_revenue[g] - reported_generator_cost[g]) for g = 1:n_g];
 
-std_sensitivity_coeffs, mean_sensitivity_coeffs = zeros(n_w, n_g), zeros(n_w, n_g);
-std_sensitivity, mean_sensitivity = zeros(n_w), zeros(n_w);
+std_sensitivity_coeffs, std_sensitivity = zeros(n_w, n_g), zeros(n_w);
+mean_sensitivity_coeffs, mean_sensitivity = zeros(n_w, n_g), zeros(n_w);
 reserve_price_est = zeros(n_w);
 for i = 1:n_w
     for k = 1:n_g
         std_sensitivity_coeffs[i, k] = std[i] * alpha_scheduled[k, i]^2 + sum((corr_mat[i, j] * std[j] * alpha_scheduled[k, j] * alpha_scheduled[k, i]) for j = 1:n_w if j != i)
         std_sensitivity[i] += std_sensitivity_coeffs[i, k] * (2 * C_Q[k] + (phi / sqrt(alpha_scheduled[k,:]'*cov_mat*alpha_scheduled[k,:])) * (dual_min_prod[k] + dual_max_prod[k]))
-        mean_sensitivity_coeffs[i, k] = alpha_scheduled[k, i] * (-C_L[k] + 2 * C_Q[k] * (-p_scheduled[k] + sum(alpha_scheduled[k, j] * mu[j] for j = 1:n_w)) + dual_min_prod[k] - dual_max_prod[k])
-        mean_sensitivity[i] += mean_sensitivity_coeffs[i, k]
+        #mean_sensitivity_coeffs[i, k] = alpha_scheduled[k, i] * (-C_L[k] + 2 * C_Q[k] * (-p_scheduled[k] + sum(alpha_scheduled[k, j] * mu[j] for j = 1:n_w)) + dual_min_prod[k] - dual_max_prod[k])
+        #mean_sensitivity[i] += mean_sensitivity_coeffs[i, k]
     end
+    #reserve_price_est[i] = mu[i] * mean_sensitivity[i] + std[i] * std_sensitivity[i];
     reserve_price_est[i] = mu[i] * mean_sensitivity[i] + std[i] * std_sensitivity[i];
 end
+
+gamma, eta = zeros(Float64, n_w), zeros(Float64, n_w);
+gamma .= 1 ./ (2 .* (C_Q .+ dual_min_prod .* phi.^2 + dual_max_prod .* phi.^2));
+beta = 1 / sum(gamma);
+eta .= beta .* gamma;
 
 estimated_generator_profit = zeros(n_g);
 for k = 1:n_g
@@ -144,6 +149,7 @@ end
 println("\n")
 println("Power generation: ", p_scheduled)
 println("Reserve procurement: ", alpha_scheduled)
+println("Estimated reserve procurement: ", eta)
 println("Electricity price: ", electricity_price)
 println("Reserve price: ", reserve_price)
 println("Reserve price estimate: ", reserve_price_est)
